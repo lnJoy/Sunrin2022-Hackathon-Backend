7e1981fe344a19e907074828b7a5a9b1421edce5
lnJoy <lnjoy3306@gmail.com>
2022-07-16 00:46:43 +0900

    fix: 테이블 변경

--- src/database/migrations/1657899978936-CreateTable.ts
+++ src/database/migrations/1657899978936-CreateTable.ts
@@ -1,10 +1,13 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateTable1657885419634 implements MigrationInterface {
-    name = 'CreateTable1657885419634'
+export class CreateTable1657899978936 implements MigrationInterface {
+    name = 'CreateTable1657899978936'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP CONSTRAINT "FK_24e8376d0e2c371c95423c131ff"`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "photo"`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP CONSTRAINT "UQ_24e8376d0e2c371c95423c131ff"`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "locationId"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "distinction"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "contents"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "breeds"`);
@@ -12,10 +15,6 @@ export class CreateTable1657885419634 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "age"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "missingAddress"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "missingDate"`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP CONSTRAINT "UQ_24e8376d0e2c371c95423c131ff"`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "locationId"`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "locationId" integer`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD CONSTRAINT "UQ_24e8376d0e2c371c95423c131ff" UNIQUE ("locationId")`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "distinction" character varying`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "contents" text`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "breeds" character varying`);
@@ -23,11 +22,17 @@ export class CreateTable1657885419634 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "age" integer`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "missingAddress" character varying`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "missingDate" character varying`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD CONSTRAINT "FK_24e8376d0e2c371c95423c131ff" FOREIGN KEY ("locationId") REFERENCES "location_entity"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "photo" text`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "locationId" integer`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD CONSTRAINT "UQ_24e8376d0e2c371c95423c131ff" UNIQUE ("locationId")`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD CONSTRAINT "FK_24e8376d0e2c371c95423c131ff" FOREIGN KEY ("locationId") REFERENCES "location"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP CONSTRAINT "FK_24e8376d0e2c371c95423c131ff"`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP CONSTRAINT "UQ_24e8376d0e2c371c95423c131ff"`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "locationId"`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "photo"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "missingDate"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "missingAddress"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "age"`);
@@ -35,10 +40,6 @@ export class CreateTable1657885419634 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "breeds"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "contents"`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "distinction"`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP CONSTRAINT "UQ_24e8376d0e2c371c95423c131ff"`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" DROP COLUMN "locationId"`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "locationId" integer`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD CONSTRAINT "UQ_24e8376d0e2c371c95423c131ff" UNIQUE ("locationId")`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "missingDate" character varying`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "missingAddress" character varying`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "age" integer`);
@@ -46,7 +47,10 @@ export class CreateTable1657885419634 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "breeds" character varying`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "contents" text`);
         await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "distinction" character varying`);
-        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD CONSTRAINT "FK_24e8376d0e2c371c95423c131ff" FOREIGN KEY ("locationId") REFERENCES "location_entity"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "locationId" integer`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD CONSTRAINT "UQ_24e8376d0e2c371c95423c131ff" UNIQUE ("locationId")`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD "photo" text`);
+        await queryRunner.query(`ALTER TABLE "road_cat_post" ADD CONSTRAINT "FK_24e8376d0e2c371c95423c131ff" FOREIGN KEY ("locationId") REFERENCES "location"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
 }
